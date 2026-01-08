8563d5c6fb459c5be1b871966e463586b7ae016b
Arunkumar-gitacc <bollapallyarunsai812@gmail.com>
2026-01-08 18:22:57 +0530

    test

--- src/home/SearchDonar.js
+++ src/home/SearchDonar.js
@@ -113,7 +113,7 @@ const SearchDonar = (props) => {
             if (!errors.country && !errors.state && !errors.district && !errors.cityOrVillage && !errors.pinCode && !errors.bloodGroup) {
                 try {
                     // Make a GET request to verify the data
-                    const response = await axios.get('http://${API_BASE_URL}:8080/userdetails/getAllUsers');
+                    const response = await axios.get(`${API_BASE_URL}/userdetails/getAllUsers`);
                 
                     if (response.status === 200) {
                         const users = response.data;
