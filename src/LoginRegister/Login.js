1abc696ca92ea80df4de590cf73afa78a37e5df2
Arunkumar-gitacc <bollapallyarunsai812@gmail.com>
2026-01-12 16:21:32 +0530

    versions_test

--- src/LoginRegister/Login.js
+++ src/LoginRegister/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const Login = (props) => {
   const [formData, setFormData] = useState({
