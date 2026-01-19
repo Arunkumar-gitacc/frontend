92cad1c6384b3004bf3e1a0892d43b39f035cfac
Arunkumar-gitacc <bollapallyarunsai812@gmail.com>
2026-01-19 11:14:21 +0530

    deploy

--- src/LoginRegister/Register.js
+++ src/LoginRegister/Register.js
@@ -215,7 +215,7 @@ const Register = ({ onSelectChange }) => {
           try {
             const response = await axios.post(`${API_BASE_URL}/userdetails/save`, formData);
             if (response.status === 200) {
-              alert('Form Submitted Successfully1234');
+              alert('Form Submitted Successfully12345');
             } else {
               alert('There was a problem submitting the form. Please try again.');
             }
